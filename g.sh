adc98a35245f861b6cce24356f165de93ea8cee3
midasdaro <44830370+midasdaro@users.noreply.github.com>
2019-04-03 23:16:27 +0200

    Add files via upload

---
+++ g.sh
@@ -0,0 +1,11 @@
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+echo -e '\033[91m'
+cat $PREFIX/etc/g1
+echo -e '''
+ 	 \033[92m+-+-+-+ +-+-+-+-+-+-+-+-+
+	\033[92m |\033[91mT\033[92m|\033[91mh\033[92m|\033[91me\033[92m| |\033[91mT\033[92m|\033[91mo\033[92m|\033[91mr\033[92m|\033[91mt\033[92m|\033[91mu\033[92m|\033[91mr\033[92m|\033[91me\033[92m|\033[91mr\033[92m|
+	 +-+-+-+ +-+-+-+-+-+-+-+-+
+'''
+PS1='\033[92m[\033[91mRoot@\033[93mTorturer\033[40m \w\033[92m]\$ '
